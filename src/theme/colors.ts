347c91e42c47e080f671cc9716a3654aae031d38
dev-soul <87164852+dev-soul@users.noreply.github.com>
2021-10-03 14:22:22 +0600

    text color changes

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -17,16 +17,16 @@ export const brandColors = {
 export const lightColors: Colors = {
   ...baseColors,
   ...brandColors,
-  background: "#323232", //Background, the main block in the centre 
+  background: "#ffffff", //Background, the main block in the centre 
   backgroundDisabled: "#E9EAEB", //Harvest button when nothing to harvest
   contrast: "#191326", //might be the stake only slider when active? But in dark mode its called invertedContrast
-  invertedContrast: "#FFFFFF",
+  invertedContrast: "#000000",
   input: "#eeeaf4", //'stake only' slider
   inputBorder: "#cccccc",
   tertiary: "#EFF4F5", //color of highlighted box on navbar
-  text: "#FFFFFF", //color of words on home menu cards eg farms & staking
+  text: "#653101", //color of words on home menu cards eg farms & staking
   textDisabled: "#BDC2C4", //color of boxes non highlighted on menu, might actually be the unhighlighted harvest word
-  textSubtle: "#FFFFFF", //color of text on menu dropdown items
+  textSubtle: "#653101", //color of text on menu dropdown items
   borderColor: "#E9EAEB", //The small line on the left side, not important
   card: "#191919", //Top and side blocks i think
   gradients: {
@@ -40,15 +40,15 @@ export const darkColors: Colors = {
   secondary: "#e9f1fd", //color of words on top of LP staking page (Stake tokens to earn ...)
   background: "#323232",
   backgroundDisabled: "#3c3742",
-  contrast: "#FFFFFF",
+  contrast: "#653101",
   invertedContrast: "#0a0e17", //might be the stake only slider when activated
   input: "#483f3b", // 'stake only' slider
   inputBorder: "#cccccc",
   primaryDark: "#599e17",
   tertiary: "#2d2f37", //color of highlighted box on menu
-  text: "#FFFFFF", //color of words on home menu cards eg farms & staking
+  text: "#653101", //color of words on home menu cards eg farms & staking
   textDisabled: "#666171", //color of boxes non highlighted on menu
-  textSubtle: "#FFFFFF", //color of text on menu dropdown items
+  textSubtle: "#653101", //color of text on menu dropdown items
   borderColor: "#524B63", //The small line on the left side, not important
   card: "#191919",
   gradients: {
