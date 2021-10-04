a51d03540add6483d425d33ede7f8c987393e193
dev-soul <87164852+dev-soul@users.noreply.github.com>
2021-10-04 14:28:04 +0600

    social changes

--- src/widgets/Menu/config.ts
+++ src/widgets/Menu/config.ts
@@ -116,82 +116,19 @@ export const links = [
 
 export const socials = [
   {
-    label: "Github",
-    icon: "GithubIcon",
-    href: "https://github.com/martiandefi",
-  },
-  {
-    label: "Gitbook",
-    icon: "GitbookIcon",
-    href: "https://martianswap.gitbook.io/",
-  },
-  {
-    label: "Medium",
-    icon: "MediumIcon",
-    href: "https://martianswap.medium.com/",
-  },
-  {
     label: "Telegram",
     icon: "TelegramIcon",
     items: [
       {
         label: "English",
-        href: "https://t.me/martianswap",
+        href: "https://t.me/DogeCakeMoonBSC",
       },
-      // {
-      //   label: "Bahasa Indonesia",
-      //   href: "https://t.me/PancakeSwapIndonesia",
-      // },
-      // {
-      //   label: "中文",
-      //   href: "https://t.me/PancakeSwap_CN",
-      // },
-      // {
-      //   label: "Tiếng Việt",
-      //   href: "https://t.me/PancakeSwapVN",
-      // },
-      // {
-      //   label: "Italiano",
-      //   href: "https://t.me/pancakeswap_ita",
-      // },
-      // {
-      //   label: "русский",
-      //   href: "https://t.me/pancakeswap_ru",
-      // },
-      // {
-      //   label: "Türkiye",
-      //   href: "https://t.me/pancakeswapturkiye",
-      // },
-      // {
-      //   label: "Português",
-      //   href: "https://t.me/PancakeSwapPortuguese",
-      // },
-      // {
-      //   label: "Español",
-      //   href: "https://t.me/PancakeswapEs",
-      // },
-      // {
-      //   label: "日本語",
-      //   href: "https://t.me/pancakeswapjp",
-      // },
-      // {
-      //   label: "Français",
-      //   href: "https://t.me/pancakeswapfr",
-      // },
-      {
-        label: "Announcements",
-        href: "https://t.me/martianswapnews",
-      },
-      // {
-      //   label: "Whale Alert",
-      //   href: "https://t.me/PancakeSwapWhales",
-      // },
     ],
   },
   {
     label: "Twitter",
     icon: "TwitterIcon",
-    href: "https://twitter.com/MartianSWAP",
+    href: "https://twitter.com/DogeCakeMoon",
   },
 ];
 
